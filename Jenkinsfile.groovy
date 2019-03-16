9fb193a0dea4677737e2c550d743994c3addaa3b
farrukh90 <farrukhsadykov@gmail.com>
2019-03-16 11:03:03 -0500

    more instructions added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,21 @@
 node {
-    stage("Pull git")
-       git "git@github.com:farrukh90/website.git"
+    stage("Pull git"){
+        git "git@github.com:farrukh90/website.git"
+    }
+
+    stage("Install apache"){
+        sh "ssh ec2-user@${DEVIP}      sudo yum install httpd -y"
+    }
+
+    stage("Start Apache"){
+        sh "ssh ec2-user@${DEVIP}      sudo systemctl start httpd"
+    }
+
+    stage("Copy files"){
+        sh "rsync -aP --delete  index.html   ec2-user@${DEVIP}:/tmp"
+    }
+    
+    stage("Move files"){
+        sh "ssh ec2-user@${DEVIP}      sudo cp -f  /tmp/index.html  /var/www/html/index.html"
+    }
 }
